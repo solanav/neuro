f1d56c7db6e33baa10d0e470859d2735ffa791eb
v0sem <progpabsanch@gmail.com>
2021-03-17 16:27:18 +0100

    Adaline

---
+++ src/adaline.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+
+#include "neurona.h"
+#include "conexion.h"
+#include "capa.h"
+#include "red_neuronal.h"
+#include "lector.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    Lector l;
+    l.leer1((char *) "and.txt", 0.5);
+
+    // Lo sacamos del lector
+    int num_rows_entrenamiento = l.entradas_entrenamiento.size();
+    int num_entradas = l.num_entradas;
+    int num_salidas = l.num_salidas;
+
+    // Establecemos la tasa de aprendizaje
+    float tasa_aprendizaje = 0.02;
+
+    // Setup de la red
+    int umbral = 1;
+    std::vector<Neurona> entrada_raw = std::vector<Neurona>();
+    std::vector<Neurona> salida_raw = std::vector<Neurona>();
+
+    Capa capa_entrada = Capa();
+    for (int i = 0; i < num_entradas; i++)
+    {
+        Neurona tmp = Neurona(umbral, Neurona::Tipo::Sesgo);
+        entrada_raw.push_back(tmp);
+        capa_entrada.Anadir(&tmp);
+    }
+
+    Capa capa_salida = Capa();
+    for (int i = 0; i < num_salidas; i++)
+    {
+        Neurona tmp = Neurona(umbral, Neurona::Tipo::Sesgo);
+        salida_raw.push_back(tmp);
+        capa_salida.Anadir(&tmp);
+    }
+
+    capa_entrada.Conectar(&capa_salida, PESO_PEQUENO);
+
+    RedNeuronal red = RedNeuronal();
+    red.Anadir(&capa_entrada);
+    red.Anadir(&capa_salida);
+
+    // Entrenamiento
+    bool cont = true;
+    while (cont)
+    {
+        for (int i = 0; i < num_rows_entrenamiento; i++)
+        {
+            std::vector<float> entrada = l.entradas_entrenamiento[i];
+            std::vector<float> salida = l.salidas_entrenamiento[i];
+
+            for (int j = 0; j < num_entradas; j++)
+                entrada_raw[j].inicializar(entrada[j]);
+
+            std::cout << entrada_raw[1].tipo << std::endl;
+
+            red.Disparar();
+            red.Inicializar();
+            red.Propagar();
+
+            // Imprimimos la salida
+            for (int j = 0; j < num_salidas; j++)
+            {
+                float y = salida_raw[j].valor;
+                float t = salida[j];
+                
+                for (int k = 0; k < num_entradas; k++)
+                {
+                    // Actualizamos el peso
+                    float old_peso = entrada_raw[k].conexiones[j].peso;
+                    entrada_raw[k].conexiones[j].peso_anterior = old_peso;
+
+                    float new_peso = old_peso + (tasa_aprendizaje * (t-y) * entrada[k]);
+                    entrada_raw[k].conexiones[j].peso = new_peso;
+
+                    // Actualizamos el sesgo
+                    entrada_raw[k].sesgo = entrada_raw[k].sesgo + (tasa_aprendizaje * (t-y));
+                }
+            }
+        }
+
+        cont = false;
+    }
+
+    return 0;
+}
